580f3d0cb44139a209c9dd06308af7efa38ffa76
Shawn Kost <shawnmkost@gmail.com>
2021-03-12 18:50:53 +0000

    Added cron schedule to post GME and RBLX prices every hour between 8-2

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ client.on("ready", () => {
       channel.send(`Daily quote of the day: ${quote}`)
     );
   });
+  cron.schedule("0 16-22/1 * * *", async () => {
+    const channel = client.channels.cache.get("398918078655758339");
+    const price = await yahooStockPrices.getCurrentPrice("GME");
+    msg.channel.send(`GME CURRENT PRICE: $${price} 💎🙌🚀🚀🚀 `);
+    const roblox = await yahooStockPrices.getCurrentPrice("RBLX");
+    msg.channel.send(`ROBLOX CURRENT PRICE: $${roblox}`);
+  })
 });
 
 client.on("guildMemberAdd", (member) => {
